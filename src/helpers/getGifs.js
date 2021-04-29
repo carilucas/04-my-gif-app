75dceba2b735ff57863b08ed6adac684874bf7f7
carlos <carlos.mora.ram@gmail.com>
2021-04-29 14:51:12 -0500

    https corregido

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -2,7 +2,7 @@
 
 export const getGifs = async(category)=>{
       
-   const url = `http://api.giphy.com/v1/gifs/search?api_key=bRWSlDNKQ3bI7tNLcHIv9rqI5kAwGtjr&q=${encodeURI(category)}&limit=10`;
+   const url = `https://api.giphy.com/v1/gifs/search?api_key=bRWSlDNKQ3bI7tNLcHIv9rqI5kAwGtjr&q=${encodeURI(category)}&limit=10`;
 
    const resp = await fetch(url);
 
